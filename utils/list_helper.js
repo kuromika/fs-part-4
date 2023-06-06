eec58950a29011a264e94f6717a627ad520ce516
kuromika <kuromika.dev@gmail.com>
2023-06-05 22:16:07 -0500

    Test favorite blog - 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,9 +7,25 @@ const totalLikes = (blogs) => {
   const reducer = (sum, current) => sum + current.likes;
   return blogs.reduce(reducer, 0);
 };
-// ...
+
+const favoriteBlog = (blogs) => {
+  let favorite = -1;
+  let maxLikes = 0;
+
+  for (let i = 0; i < blogs.length; i += 1) {
+    if (blogs[i].likes > maxLikes) {
+      favorite = i;
+      maxLikes = blogs[i].likes;
+    }
+  }
+  if (favorite < 0) {
+    return null;
+  }
+  return blogs[favorite];
+};
 
 module.exports = {
+  favoriteBlog,
   dummy,
   totalLikes,
 };
