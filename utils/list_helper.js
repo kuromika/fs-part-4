d2d0b6597eb73466c0f7c598e7876c0899aee412
kuromika <kuromika.dev@gmail.com>
2023-06-05 22:05:08 -0500

    Test blogs total likes - 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,15 @@
 const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => {
+  if (!blogs.length) {
+    return 0;
+  }
+  const reducer = (sum, current) => sum + current.likes;
+  return blogs.reduce(reducer, 0);
+};
 // ...
 
 module.exports = {
   dummy,
+  totalLikes,
 };
